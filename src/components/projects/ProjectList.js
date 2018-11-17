182f7f31ee3947a9d762cdb54f18eafd3e6cb319
V S Bharath <bharathcse5t6@gmail.com>
2018-11-17 12:16:30 +0530

    project summary

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,9 @@
 import React from "react";
-
+import ProjectSummary from "./ProjectSummary";
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by net ninja</p>
-          <p className="grey-text">eerraju</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by net ninja</p>
-          <p className="grey-text">eerraju</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by net ninja</p>
-          <p className="grey-text">eerraju</p>
-        </div>
-      </div>
+      <ProjectSummary />
     </div>
   );
 };
