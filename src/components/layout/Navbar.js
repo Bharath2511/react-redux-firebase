eaf447e8c977c85f6ffd80af70ee30c9ba6183e2
V S Bharath <bharathcse5t6@gmail.com>
2018-11-18 10:58:42 +0530

    understanding fb auth

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
+import { connect } from "react-redux";
 
 const Navbar = () => {
   return (
@@ -17,4 +18,10 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+const mapStateToProps = state => {
+  //here state is coming from the root reducer
+  console.log(state);
+  return {};
+};
+
+export default connect(mapStateToProps)(Navbar);
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,11 +3,15 @@ import projectReducer from "./projectReducer";
 import { combineReducers } from "redux";
 //syncing our redux state(server state) with database
 import { firestoreReducer } from "redux-firestore";
+import { firebaseReducer } from "react-redux-firebase";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
-  firestore: firestoreReducer
+  //syncs the projects with redux state
+  firestore: firestoreReducer,
+  //it syncs our auth status in fire bse with redux state
+  firebase: firebaseReducer
 });
 
 export default rootReducer;
